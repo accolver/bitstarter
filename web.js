d5158557053e34bfd662f2fdf129c956530beba3
Alan Colver <accolver@gmail.com>
2013-07-10 02:46:54 +0000

    render

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   fs.readFile(__dirname + "/index.html", function(err, data) {
-    response.send(data);
+    response.render(data);
   });
 });
 
